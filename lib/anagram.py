3538a5e7e20482ba5925f2c6388492a5c11be177
felix <felixmonari400@gmail.com>
2024-09-10 04:14:08 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+# lib/anagram.py
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def is_anagram(self, other_word):
+        """Check if the given word is an anagram of the instance's word."""
+        return sorted(self.word) == sorted(other_word)
+
+    def match(self, word_list):
+        """Return a list of words from word_list that are anagrams of the instance's word."""
+        return [word for word in word_list if self.is_anagram(word)]
